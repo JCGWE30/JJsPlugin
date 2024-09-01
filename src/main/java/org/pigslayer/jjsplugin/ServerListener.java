ef1a1188152d03cc65c8e1866d6a9aec3122a141
JC GWE <gwejc69@gmail.com>
2024-08-31 23:47:10 -0500

    port fix?

--- src/main/java/org/pigslayer/jjsplugin/ServerListener.java
+++ src/main/java/org/pigslayer/jjsplugin/ServerListener.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 
 public class ServerListener {
     public static void startServer(){
-        port(8080);
+        port(8081);
 
         post("/message",(request,response) -> {
             String message = request.body();
